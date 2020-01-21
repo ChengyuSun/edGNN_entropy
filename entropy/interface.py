5286c3af4d02928644490b2bcd7ca02689168f18
scy <1294974924@qq.com>
2020-01-21 12:37:28 +0800

    1.21预处理完成之后model存在问题

--- entropy/interface.py
+++ entropy/interface.py
@@ -1,11 +1,11 @@
 from entropy.CountMotif_nr import countMotifs
 from entropy.Entropy import graphEntropy
 from entropy.edge_entropy import edgeEntropy
-# from entropy.countedge import countEdge
+from entropy.countedge import countEdge
 
-g_xlsx='./data/graph1.xlsx'
-g_csv='./data2/graphfile.csv'
-def preporcess(graphfile_xlsx)
-print(edgeEntropy(graphEntropy(countMotifs(g_xlsx))))
+def writeEdgeEntropy(graphfile_xlsx):
+    countEdge(graphfile_xlsx)
+    edgeEntropy(graphEntropy(countMotifs(graphfile_xlsx)))
+
 
 
