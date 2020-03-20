76aca7f122e056fc7952eee9afd39ed83dec6d17
scy <1294974924@qq.com>
2020-03-20 22:33:24 +0800

    0320测试ptc_entropy

--- core/models/model.py
+++ core/models/model.py
@@ -187,10 +187,11 @@ class Model(nn.Module):
         elif isinstance(self.embed_nodes, torch.Tensor):
             #node_features = self.embed_nodes[self.g.ndata[GNN_NODE_LABELS_KEY]]
             label=self.g.ndata[GNN_NODE_LABELS_KEY].view(-1,1).long().cuda()
-            o=torch.ones(len(label),1).long().cuda()
-            label=label-o
-            # print('node_dim:'+str(self.node_dim))
-            # print('label'+str(label))
+
+            #NCI1 need the below 2 lines
+            #o = torch.ones(len(label), 1).long().cuda()
+            #label=label-o
+
             zeros=torch.zeros(len(self.g.ndata[GNN_NODE_LABELS_KEY]), self.node_dim).cuda()
             node_features=zeros.scatter_(1 , label, 1)
         else:
@@ -207,8 +208,9 @@ class Model(nn.Module):
             #edge_features = self.embed_edges[self.g.edata[GNN_EDGE_LABELS_KEY].type(torch.uint8)]
 
             #turn label into one-hot
-            label=self.g.edata[GNN_EDGE_LABELS_KEY].view(-1,1)
-            edge_features=torch.zeros(len(self.g.edata[GNN_EDGE_LABELS_KEY]),self.edge_dim).scatter_(1,label.long(),1)
+            label=self.g.edata[GNN_EDGE_LABELS_KEY].view(-1,1).long().cuda()
+            zeros = torch.zeros(len(self.g.edata[GNN_EDGE_LABELS_KEY]),self.edge_dim).cuda()
+            edge_features=zeros.scatter_(1,label,1)
 
             # print("edge_features:" + str(edge_features.size()))
             # print("self.g.edata[GNN_EDGE_FEAT_KEY]:" + str(self.g.edata[GNN_EDGE_FEAT_KEY].size()))
