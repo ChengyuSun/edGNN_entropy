406f627d41c0be24428af952da394220c9703c2f
scy <1294974924@qq.com>
2020-03-21 20:28:15 +0800

    0321测试nci109_entropy,n_rels缺省值改为1

--- core/models/model.py
+++ core/models/model.py
@@ -67,7 +67,7 @@ def layer_build_args(node_dim, edge_dim, n_classes, layer_params, mode):
 
 class Model(nn.Module):
 
-    def __init__(self, g, config_params, n_classes=None, n_rels=1, n_entities=None, is_cuda=False, mode=NODE_CLASSIFICATION):
+    def __init__(self, g, config_params, n_classes=None, n_rels=None, n_entities=None, is_cuda=False, mode=NODE_CLASSIFICATION):
         """
         Instantiate a graph neural network.
 
@@ -90,6 +90,8 @@ class Model(nn.Module):
         self.mode = mode
         self.config_params = config_params
         self.n_rels = n_rels
+        if self.n_rels==None:
+            self.n_rels=1
         self.n_classes = n_classes
         self.n_entities = n_entities
         self.g = g
