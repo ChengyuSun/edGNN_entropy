9f54965bc4d84bed6c1470f5bf950da8ab159b42
scy <1294974924@qq.com>
2020-03-21 08:14:40 +0800

    0321测试mutag_entropy

--- core/app.py
+++ core/app.py
@@ -209,6 +209,6 @@ class App:
 
         print("\nTest Accuracy {:.4f}".format(acc))
 
-        with open('./ptc_entropy_acc.txt', 'a+') as f:
+        with open('./mutag_entropy_acc.txt', 'a+') as f:
             f.write(str(acc) + '\n')
         return acc
