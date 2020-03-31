74107732c1c55fb49ac510f7879df123a0755319
scy <1294974924@qq.com>
2020-03-31 14:43:51 +0800

    0331-no-entropy

--- bin/pre_cora.py
+++ bin/pre_cora.py
@@ -7,7 +7,7 @@ from dgl import DGLGraph
 
 from core.data.constants import GRAPH, LABELS, TRAIN_MASK, TEST_MASK, VAL_MASK, N_CLASSES
 from core.data.utils import complete_path, load_pickle, save_pickle
-from core.models.constants import GNN_NODE_ATTS_KEY,GNN_EDGE_FEAT_KEY
+from core.models.constants import GNN_NODE_ATTS_KEY
 from entropy.utils import read_adjMatrix_csv
 
 
@@ -84,7 +84,7 @@ def save_cora(out_folder):
                 g.add_edges(i, j)
                 edge_feature.append(edge_feature_all[i*N+j])
 
-    g.edata[GNN_EDGE_FEAT_KEY] =torch.from_numpy(np.array(edge_feature))
+    #g.edata[GNN_EDGE_FEAT_KEY] =torch.from_numpy(np.array(edge_feature))
 
 
     #save
