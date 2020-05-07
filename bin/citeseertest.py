b1919dc91deef503f7b624dce9800cb2aa9b91ac
scy <1294974924@qq.com>
2020-05-07 21:29:47 +0800

    0507

--- bin/citeseertest.py
+++ bin/citeseertest.py
@@ -25,18 +25,23 @@ adj = np.zeros((nodN, nodN))
 cite_file = open('./citeseer.cites', "r").readlines()
 for line in cite_file:
     vector = [x for x in line.strip('\n').split('\t')]
-    if not node_name_to_index.__contains__(vector[0]) or not node_name_to_index.__contains__(vector[1]):
+    if not node_name_to_index.__contains__(vector[0]) \
+            or not node_name_to_index.__contains__(vector[1])\
+            or vector[0]==vector[1]:
         continue
 
     start = node_name_to_index[vector[0]]
     end = node_name_to_index[vector[1]]
-
-    #print(str(start) + '->' + str(end))
+    # if start==end:
+    #   print(str(vector[0])+'->'+str(vector[1]))
+    # #print(str(start) + '->' + str(end))
     adj[start][end] = 1
     adj[end][start] = 1
     edge_num+=2
 
 print('edge_num:',edge_num)
+
+
 
 with open('./citeseer_adj.txt', 'w') as adj_file:
     for i in range(nodN):
