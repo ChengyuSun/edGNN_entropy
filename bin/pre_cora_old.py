f19f4018824e6fbda05b5947eef440cb2bd1501e
scy <1294974924@qq.com>
2020-05-08 20:58:24 +0800

    0508

--- bin/pre_cora_old.py
+++ bin/pre_cora_old.py
@@ -7,7 +7,7 @@ from dgl import DGLGraph
 
 from core.data.constants import GRAPH, LABELS, TRAIN_MASK, TEST_MASK, VAL_MASK, N_CLASSES
 from core.data.utils import complete_path, load_pickle, save_pickle
-from core.models.constants import GNN_NODE_ATTS_KEY
+from core.models.constants import GNN_NODE_ATTS_KEY,GNN_EDGE_FEAT_KEY
 from entropy.utils import read_adjMatrix_txt
 
 
@@ -53,18 +53,18 @@ def save_cora(out_folder):
     print('g.ndata[GNN_NODE_ATTS_KEY]:',g.ndata[GNN_NODE_ATTS_KEY].size())
 
     #edge
-    # edge_entropy=[]
-    # edge_entropy_file=open('../bin/preprocessed_data/citeseer/citeseer/citeseer_edge_entropy.txt',"r").readlines()
-    # for line in edge_entropy_file:
-    #     vector2 = [float(x) for x in line.strip('\n').strip(',').split(",")]
-    #     # sum=0
-    #     # for item in vector2:
-    #     #     sum+=item
-    #     # edge_entropy.append(sum)
-    #     edge_entropy.append(vector2)
-    #
-    # edge_entropy=torch.from_numpy(np.array(edge_entropy)).view(nodN*nodN,8)
-    # print('edge_entropy:',edge_entropy.size())
+    edge_entropy=[]
+    edge_entropy_file=open('../bin/preprocessed_data/citeseer/citeseer/citeseer_edge_entropy.txt',"r").readlines()
+    for line in edge_entropy_file:
+        vector2 = [float(x) for x in line.strip('\n').strip(',').split(",")]
+        sum=0
+        for item in vector2:
+            sum+=item
+        edge_entropy.append(sum)
+        #edge_entropy.append(vector2)
+
+    edge_entropy=torch.from_numpy(np.array(edge_entropy)).view(nodN*nodN,1)
+    print('edge_entropy:',edge_entropy.size())
 
     # attention_sum=torch.zeros(nodN,nodN).view(nodN*nodN,1)
     # for i in range(8):
@@ -86,9 +86,9 @@ def save_cora(out_folder):
     # attention_average = (attention_sum * (1 / 8)).unsqueeze(-1).view(nodN * nodN, 1)
 
     #edge_feature_all=torch.mul(attention_average,edge_entropy).numpy()
-    edge_feature_all=torch.randn(nodN * nodN, 8).numpy()
+    #edge_feature_all=torch.randn(nodN * nodN, 8).numpy()
 
-    #edge_feature_all = edge_entropy.numpy()
+    edge_feature_all = edge_entropy.numpy()
 
     edge_feature=[]
     adj, N = read_adjMatrix_txt('./preprocessed_data/citeseer/citeseer/citeseer_adj.txt')
@@ -99,8 +99,8 @@ def save_cora(out_folder):
                 edge_feature.append(edge_feature_all[i*N+j])
                 #print('edge_feature_all[i*N+j]:',edge_feature_all[i*N+j])
 
-    # g.edata[GNN_EDGE_FEAT_KEY] = torch.from_numpy(np.array(edge_feature))
-    # print('g.edata[GNN_EDGE_FEAT_KEY]',g.edata[GNN_EDGE_FEAT_KEY].size())
+    g.edata[GNN_EDGE_FEAT_KEY] = torch.from_numpy(np.array(edge_feature))
+    print('g.edata[GNN_EDGE_FEAT_KEY]',g.edata[GNN_EDGE_FEAT_KEY].size())
 
     #save
     save_pickle(g, complete_path(out_folder, GRAPH))
