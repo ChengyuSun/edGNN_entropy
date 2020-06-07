b163e2d57ac6d1dce1519a58f78960ea0071f0a8
scy <1294974924@qq.com>
2020-06-07 18:51:08 +0800

    0607

--- bin/pre_cora_old.py
+++ bin/pre_cora_old.py
@@ -43,13 +43,13 @@ def save_cora(out_folder):
     edge_entropy_file = open('../bin/preprocessed_data/citeseer/citeseer/citeseer_edge_entropy.txt', "r").readlines()
     for line in edge_entropy_file:
         vector2 = [float(x) for x in line.strip('\n').strip(',').split(",")]
-        # sum=0
-        # for item in vector2:
-        #     sum+=item
-        # edge_entropy.append(sum)
-        edge_entropy.append(vector2)
+        sum=0
+        for item in vector2:
+            sum+=item
+        edge_entropy.append(sum)
+        #edge_entropy.append(vector2)
 
-    edge_entropy=torch.from_numpy(np.array(edge_entropy)).view(nodN*nodN,8)
+    edge_entropy=torch.from_numpy(np.array(edge_entropy)).view(nodN*nodN,1)
     print('edge_entropy:',edge_entropy.size())
 
     edge_feature_all = edge_entropy.numpy()
