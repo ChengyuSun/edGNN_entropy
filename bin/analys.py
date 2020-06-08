668c02f1704971e67c7c6d925c9642155c50a1b8
scy <1294974924@qq.com>
2020-06-08 11:10:37 +0800

    0607

--- bin/analys.py
+++ bin/analys.py
@@ -1,3 +1,4 @@
+import copy
 import os
 
 import matplotlib.pyplot as plt
@@ -13,7 +14,7 @@ GRAPH_ID_SUFFIX = '_graph_indicator.txt'
 
 def ana():
     data = dict()
-    dataset_name='MUTAG'
+    dataset_name='NCI1'
     dirpath = './preprocessed_data/{}/unzipped/{}'.format(dataset_name,dataset_name)
     for f in os.listdir(dirpath):
         if "README" in f or '.txt' not in f:
@@ -52,11 +53,11 @@ def ana():
         node_labels=data[NODE_LABELS_SUFFIX][node_ids]
 
         draw_graph(temp_A,node_labels,temp_nodN,data[GRAPH_LABELS_SUFFIX][g_id])
-
+        #
         node_index_begin += temp_nodN
 
         b+=1
-        if b>10:
+        if b>3:
             break
 
 
@@ -81,12 +82,39 @@ def draw_graph(adj,node_labels,nodN,label):
 
 
 def count_perssad_3(adj,node_labels,nodN):
+    motif_count=0
     for i in range(nodN):
-        for j in range(nodN):
-            if node_labels[i]==1 :
-                return
+        if node_labels[i]==1 and np.sum(adj[i])>1:
+            x = np.nonzero(adj[i])
+            green_count=0
+            for j in x[0]:
+                if node_labels[j]==2:
+                    green_count+=1
+            if green_count==2:
+                motif_count+=1
+
+    return motif_count
 
 
-    return
+def red_degree(adj,node_labels,target):
+    count=0
+    x = np.nonzero(adj[target])
+    for i in x[0]:
+        if node_labels[i]==0:
+            count+=1
+    return count
+
+def count_perssad_6_ring(adj,node_labels,nodN):
+    temp_adj=copy.copy(adj)
+    motif_count=0
+    for i in range(nodN):
+        if node_labels[i]!=0:#not red node
+            temp_adj[i].fill(0)
+            temp_adj[:,i].fill(0)
+
+
+
+
+    return motif_count
 
 ana()
