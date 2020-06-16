e60f4a87c4cff6a7742a7106637faf66e0b56d75
scy <1294974924@qq.com>
2020-06-16 17:28:39 +0800

    0616

--- bin/pre_pub.py
+++ bin/pre_pub.py
@@ -5,8 +5,7 @@ import torch
 def read_label():
     label_dir=dict()
     labels=[]
-    #../bin/preprocessed_data/pub/pub_label.txt
-    node_label_file = open('D:/1科研/data/pub/pub_label.txt', "r").readlines()
+    node_label_file = open('../bin/preprocessed_data/pub/pub_label.txt', "r").readlines()
     for line in node_label_file:
         vector = [float(x) for x in line.strip('\n').split(" : ")]
         label_dir[vector[0]]=vector[1]
